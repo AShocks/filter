59581db4c50092c8fe026cd999ec7b9df9e5057d
AShocks <oshaughnessy.adrian@gmail.com>
2022-06-24 13:42:42 +0000

    Initial commit

--- filter.js
+++ filter.js
@@ -1,57 +1,29 @@
-  
-/**
- * To run this file in Gitpod, use the 
- * command node filter.js in the terminal
- */
-
-
-// Simple Filtering
-const people = [
+let students = [
   {
-    name: 'Michael',
-    age: 23,
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
   },
   {
-    name: 'Lianna',
-    age: 16,
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 80, art: 95},
   },
   {
-    name: 'Paul',
-    age: 18,
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 84, maths: 97, art: 95},
   },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 67, english: 87, art: 95},
+  }
 ];
 
-
-// Complex Filtering
-const students = [
-  {
-    id: 1,
-    name: 'Mark',
-    profession: 'Developer',
-    skills: [
-      { name: 'javascript', yrsExperience: 1 },
-      { name: 'html', yrsExperience: 5 },
-      { name: 'css', yrsExperience: 3 },
-    ]
-  },
-  {
-    id: 2,
-    name: 'Ariel',
-    profession: 'Developer',
-    skills: [
-      { name: 'javascript', yrsExperience: 0 },
-      { name: 'html', yrsExperience: 4 },
-      { name: 'css', yrsExperience: 2 },
-    ]
-  },
-  {
-    id: 3,
-    name: 'Jason',
-    profession: 'Designer',
-    skills: [
-      { name: 'javascript', yrsExperience: 1 },
-      { name: 'html', yrsExperience: 1 },
-      { name: 'css', yrsExperience: 5 },
-    ]
-  },
-];
+const topMaths = students.filter(student => student.results.maths >= 90)
+console.log(topMaths)
